f61bdc16538419386daff2820540fe40d1d27ba4
Gingtea <huynhkhoa03012004@gmail.com>
2024-11-25 19:58:47 +0700

    Update homepage in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Layout from "./components/Layout"
-import { Printers } from "./components/printers"
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import { Printers } from "./components/Printers";
+import Home from "./components/Home" // Thêm trang HomePage
+// import OrderPage from "./pages/OrderPage"; // Thêm trang OrderPage
+// import ReportPage from "./pages/ReportPage"; // Thêm trang ReportPage
+import './App.css';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="printers" element={<Printers />}></Route>
-        </Route>
-      </Routes>
-      
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    {/* <Route path="order" element={<Order />} />
+                    <Route path="report" element={<Report />} />
+                    <Route path="setting" element={<Printers />} />
+                    <Route path="users" element={<Users />} /> */}
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
