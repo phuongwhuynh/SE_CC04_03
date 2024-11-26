ad7a85be0c1e3691595ef6b485249294409df587
Gingtea <huynhkhoa03012004@gmail.com>
2024-11-26 09:55:19 +0700

    Change Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import Home from "./components/Home"
+import Order from "./components/Order";
 import { Printers } from "./components/Printers";
-import Home from "./components/Home" // Thêm trang HomePage
-// import OrderPage from "./pages/OrderPage"; // Thêm trang OrderPage
-// import ReportPage from "./pages/ReportPage"; // Thêm trang ReportPage
+import Users from "./components/Users";
+import Report from "./components/Report";
 import './App.css';
 
 function App() {
@@ -12,10 +13,10 @@ function App() {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
-                    {/* <Route path="order" element={<Order />} />
+                    <Route path="order" element={<Order />} />
                     <Route path="report" element={<Report />} />
                     <Route path="setting" element={<Printers />} />
-                    <Route path="users" element={<Users />} /> */}
+                    <Route path="users" element={<Users />} />
                 </Route>
             </Routes>
         </BrowserRouter>
