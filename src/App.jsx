2028dceccb609b77995a7a390f0b9055e16368fe
Tea314 <146411643+Tea314@users.noreply.github.com>
2024-12-01 22:03:43 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout"
 import { Printers } from "./components/Setting"
@@ -25,31 +24,6 @@ function App() {
       </BrowserRouter>
     </PrimeReactProvider>
   )
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./components/Home"
-import Order from "./components/Order";
-import { Printers } from "./components/Printers";
-import Users from "./components/Users";
-import Report from "./components/Report";
-import './App.css';
-
-function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="order" element={<Order />} />
-                    <Route path="report" element={<Report />} />
-                    <Route path="setting" element={<Printers />} />
-                    <Route path="users" element={<Users />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
->>>>>>> 4d40667d93d9c684b0f4c9c83c2164a1c7dcc1b9
 }
 
 export default App;
